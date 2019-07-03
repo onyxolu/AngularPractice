21a959af79d583aa2c9a97482d8c77071716f978
olumide ogundare <olumide.ogundare@interswitchgroup.com>
2019-07-03 08:58:08 +0100

    added a provider for the product service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+    styleUrls: ['./product-list.component.css'],
+    providers: [ProductService]
 })
 
 export class ProductListComponent implements OnInit{

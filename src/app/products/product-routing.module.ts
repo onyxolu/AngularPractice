322acec5e7e440830266c52463bfd297de7183a9
olumide ogundare <olumide.ogundare@interswitchgroup.com>
2019-07-09 10:30:22 +0100

    refactored the products routing for best practice

--- src/app/products/product-routing.module.ts
+++ src/app/products/product-routing.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailGuard } from './product-detail.guard';
 import { ProductDetailComponent } from './product-detail.component';
+
+const routes: Routes = [
+  { 
+    path: 'products',
+    component: ProductListComponent
+  },
+  { 
+    path: 'products/:id', 
+    canActivate: [ProductDetailGuard], 
+    component: ProductDetailComponent
+  },
+]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      { path: 'products', component: ProductListComponent},
-      { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent},
-    ]),
+    RouterModule.forChild(routes),
   ],
   exports: [RouterModule]
 })
